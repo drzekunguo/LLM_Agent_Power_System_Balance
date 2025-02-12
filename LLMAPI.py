177ab7631959778d097a3c83f10ecf80f27019e2
Zekun Guo <167107324+drzekunguo@users.noreply.github.com>
2025-02-12 23:44:17 +0000

    new

--- LLMAPI.py
+++ LLMAPI.py
@@ -1,6 +1,4 @@
 import os
 
-os.environ['AZURE_OPENAI_API_KEY'] = 'Your API key'
-os.environ['AZURE_OPENAI_ENDPOINT'] = 'Your Endpoint'
-os.environ['AZURE_API_VERSION'] = 'Your API Version'
-os.environ['AZURE_DEPLOYMENT_NAME'] = 'Your Deploy Name'
+os.environ['OPENAI_API_KEY'] = 'Your API key'
+os.environ['OPENAI_ENDPOINT'] = 'Your Endpoint'
